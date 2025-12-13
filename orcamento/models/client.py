a34ea15765c4fdf9d42d55857dd60fd1c89428b0
Ch3did <kphch3did@gmail.com>
2025-12-13 20:01:20 -0300

    pre-commit

--- orcamento/models/client.py
+++ orcamento/models/client.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Client(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField(blank=True, null=True)
--- orcamento/models/service.py
+++ orcamento/models/service.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Service(models.Model):
     name = models.CharField(max_length=255, blank=True, null=True)
     description = models.TextField(max_length=1000)
